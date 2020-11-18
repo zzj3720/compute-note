25eda39922add27206bbc2857c43ca48b801a9a5
zuozijian <zuozijian@kuaishou.com>
2020-11-18 20:08:39 +0800

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
-import { defineComponent } from "vue";
+import {defineComponent, reactive} from "vue";
+import {v1} from "uuid";
 
-export const App = defineComponent(()=>{
-    return ()=><div>
-        123
+const rootId = v1();
+
+interface Instance {
+    id: string;
+    parentId: string;
+    value: string;
+    children:string[];
+}
+
+export const App = defineComponent(() => {
+    const id = v1()
+    const instanceMap = reactive<Record<string, Instance>>({
+        [id]: {
+            id,
+            parentId:rootId,
+            value: '123',
+            children:[]
+        }
+    })
+    const roots = reactive<string[]>([id])
+    return () => <div>
+        {roots.map(id=>{
+            return <div key={id} style={{
+                display: 'flex'
+            }}>
+                <div contenteditable>
+                    {instanceMap[id].value}
+                </div>
+                <div>{instanceMap[id].value}</div>
+            </div>
+        })}
     </div>
 })
